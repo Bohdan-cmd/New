63e360d8968a472d0e2e4f2ca44cae8ccca947e8
Bohdan <deniel32.5@gmail.com>
2020-02-17 15:42:26 +0200

    Это мой первый коммит

---
+++ JS/jsfile.js
@@ -0,0 +1,23 @@
+let a = 5;
+console.log(a);
+
+let autor = {
+    name: 'bohdan',
+    age: 19,
+    isMarried: false,
+    hobbie: 'programming'
+};
+let incr = 10,
+    decr = 10;
+    
+incr++;
+++decr;
+console.log(incr);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2); 
+
+let isChecked = true;
+let isClose = true;
+
+console.log(isChecked || isClose);
